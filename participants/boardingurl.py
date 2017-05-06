581eafef0e6450c8aafec8959446c86b33daecb8
Henri Bergius <henri.bergius@iki.fi>
2017-05-07 01:58:53 +0200

    Participant for generating greetings to arriving users

---
+++ participants/boardingurl.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+
+import msgflo
+
+class BoardingUrl(msgflo.Participant):
+  def __init__(self, role):
+    d = {
+      'component': 'boardingurl',
+      'label': 'Generate greeting URL for crew members that are boarding',
+      'icon': 'check',
+      'inports': [
+        { 'id': 'in', 'type': 'object' },
+      ],
+      'outports': [
+        { 'id': 'out', 'type': 'string' },
+      ],
+    }
+    msgflo.Participant.__init__(self, d, role)
+
+  def process(self, inport, msg):
+    url = 'https://c-beam.cbrp3.c-base.org/welcome/%s' % msg.data['user']
+    self.send('out', url)
+    self.ack(msg)
+
+if __name__ == '__main__':
+  msgflo.main(BoardingUrl)
