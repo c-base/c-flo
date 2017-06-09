2e3008f6b1829f6047eec457d3e9c5970b74ccda
cannonerd <ihmis.suski@gmail.com>
2017-06-09 21:04:30 +0200

    Colours to heatmap

--- components/OnlineHeatmap.py
+++ components/OnlineHeatmap.py
@@ -17,7 +17,23 @@ class OnlineHeatmap(msgflo.Participant):
     msgflo.Participant.__init__(self, d, role)
 
   def generate_color(self, online):
-    return [128, 0, 0]
+    length = len(online)
+    if length < 5:
+      return [0,31,151]
+    if length < 10:
+      return [2,126,199]
+    if length < 15:
+      return [17,255,243]
+    if length < 20:
+      return [162,255,95]
+    if length < 25:
+      return [255,251,0]
+    if length < 30:
+      return [253,152,0]
+    if length < 35:
+      return [250,21,0]
+   
+    return [128,0,0]
     
     
   def process(self, inport, msg):
