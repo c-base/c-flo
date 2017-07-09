9755d8ea4d8547b4c410d54cd8ac654cec670e79
Anne-Julia Scheuermann <annejulia@gmail.com>
2017-07-09 18:04:58 +0200

    c_leuse: added dht22 sensor

---
+++ devices/c_leuse/c_leuse.ino
@@ -0,0 +1,101 @@
+#include "config.h"
+
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
+
+// MsgFlo communications
+#include <PubSubClient.h>
+#include <Msgflo.h>
+
+// Environment sensor
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN            D4
+#define DHTTYPE           DHT22
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+struct Config {
+  const String prefix = "sensor/";
+  const String role = "c_leuse";
+
+  const char *wifiSsid =     WIFI_SSID;
+  const char *wifiPassword = WIFI_PASSWORD;
+
+  const char *mqttHost =     MQTT_HOST;
+  const int mqttPort =       MQTT_PORT;
+
+  const char *mqttUsername = NULL;
+  const char *mqttPassword = NULL;
+} cfg;
+
+WiFiClient wifiClient;
+PubSubClient mqttClient;
+msgflo::Engine *engine;
+msgflo::OutPort *tempPort;
+msgflo::OutPort *humPort;
+
+auto participant = msgflo::Participant("c-base/c_leuse_sensor", cfg.role);
+long nextEnvCheck = 0;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  delay(100);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  Serial.printf("Configuring wifi: %s\r\n", cfg.wifiSsid);
+  WiFi.begin(cfg.wifiSsid, cfg.wifiPassword);
+
+  // Provide a Font Awesome (http://fontawesome.io/icons/) icon for the component
+  participant.icon = "wrench";
+
+  mqttClient.setServer(cfg.mqttHost, cfg.mqttPort);
+  mqttClient.setClient(wifiClient);
+
+  String clientId = cfg.role;
+  clientId += WiFi.macAddress();
+
+  engine = msgflo::pubsub::createPubSubClientEngine(participant, &mqttClient, clientId.c_str(), cfg.mqttUsername, cfg.mqttPassword);
+  
+  tempPort = engine->addOutPort("temperature", "number", cfg.prefix+cfg.role+"/temperature");
+  humPort = engine->addOutPort("humidity", "number", cfg.prefix+cfg.role+"/humidity");
+}
+
+void loop() {
+  static bool connected = false;
+
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!connected) {
+      Serial.printf("Wifi connected: ip=%s\r\n", WiFi.localIP().toString().c_str());
+    }
+    connected = true;
+    engine->loop();
+  } else {
+    if (connected) {
+      connected = false;
+      Serial.println("Lost wifi connection.");
+    }
+  }
+
+  if (millis() > nextEnvCheck) {
+    // Read DHT
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (!isnan(event.temperature)) {
+      tempPort->send(String(event.temperature));
+    }
+    dht.humidity().getEvent(&event);
+    if (!isnan(event.relative_humidity)) {
+      humPort->send(String(event.relative_humidity));
+    }
+
+    nextEnvCheck += 30000;
+  }
+}
+
+
