fe9e96359e4a37d06b2b87d166da09266811cf4f
Henri Bergius <henri.bergius@iki.fi>
2017-07-14 23:40:46 +0200

    Integer, not boolean

--- devices/c_lab/c_lab_lightsensor.ino
+++ devices/c_lab/c_lab_lightsensor.ino
@@ -55,7 +55,7 @@ void setup() {
 
   engine = msgflo::pubsub::createPubSubClientEngine(participant, &mqttClient, clientId.c_str(), cfg.mqttUsername, cfg.mqttPassword);
 
-  c_base_light = engine->addOutPort("light", "boolean", cfg.prefix+cfg.role+"/light");
+  c_base_light = engine->addOutPort("light", "int", cfg.prefix+cfg.role+"/light");
 
   Serial.printf("Light sensor pin: %d\r\n", cfg.lightPin);
 
