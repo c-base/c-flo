e97af5612c04ed9c167300d089cc41f840941cb4
Uwe Kamper <me@uwekamper.de>
2017-11-30 17:44:07 +0100

    lxdm/lightdm split up because of nerdctrl

--- ansible/roles/raspbian/templates/run_chromium.sh
+++ ansible/roles/raspbian/templates/run_chromium.sh
@@ -4,11 +4,17 @@
     sleep 2
 
     if [ -d /home/{{ display_user }}/chromium-defaults ]; then
-        cp -a /home/{{ display_user }}/chromium-defaults /home/pi/.config/chromium
+        cp -a /home/{{ display_user }}/chromium-defaults /home/{{ display_user }}/.config/chromium
     fi
 
     sleep 2
+    {% if use_kiosk %}
     chromium-browser --kiosk --no-first-run \
-        --home-page "http://c-flo/infodisplay/?msgflo_role={{ ansible_hostname }}&{% for url in urls%}msgflo_urls={{ url|urlencode }}&{% endfor %}" \
+        --home-page "http://c-flo/infodisplay/?msgflo_role={{ msgflo_role }}&{% for url in urls%}msgflo_urls={{ url|urlencode }}&{% endfor %}" \
         --disk-cache-dir=/dev/null --disk-cache-size=52428800
+    {% else %}
+    chromium-browser --no-first-run \
+        --app="http://c-flo/infodisplay/?msgflo_role={{ msgflo_role }}&{% for url in urls%}msgflo_urls={{ url|urlencode }}&{% endfor %}" \
+        --disk-cache-dir=/dev/null --disk-cache-size=52428800
+    {% endif %}
 # done
