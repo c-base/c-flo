3d081f8b8c85c9aeb8e26fb088b80abf491ec229
Henri Bergius <henri.bergius@iki.fi>
2018-02-16 23:58:28 +0100

    Go contrast/variant every other

--- components/farbhue.py
+++ components/farbhue.py
@@ -24,11 +24,12 @@ class farbhue(msgflo.Participant):
         # Convert farbgeber to Hue xy
         v1xy = converter.rgb_to_xy(msg.data['v1'][0], msg.data['v1'][1], msg.data['v1'][2])
         cxy = converter.rgb_to_xy(msg.data['c'][0], msg.data['c'][1], msg.data['c'][2])
-        contrasted = False
+        contrasted = True
         # Send to all lights. First light found gets contrast color
         for lightId in bridge.lights():
             if contrasted:
                 bridge.lights[lightId].state(xy=v1xy)
+                contrasted = False
             else:
                 contrasted = True
                 bridge.lights[lightId].state(xy=cxy)
