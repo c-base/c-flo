076575b49811be2e7b8e1b5ab03a86239e63eee1
Henri Bergius <henri.bergius@iki.fi>
2018-08-15 20:46:58 +0200

    Refactor to general event color component

--- components/EventColors.py
+++ components/EventColors.py
@@ -2,17 +2,10 @@
 import gevent
 import msgflo
 
-def hasCryptoParty(events):
-  for event in events:
-    if event['summary'].lower() == 'cryptoparty':
-      return True
-  return False
-
-#CryptoParty colors: purple(217/0/255 | d900ff) && pink(255/0/152 | ff0098)
-cryptoPartyPurple = [217, 0, 255]
-cryptoPartyPink   = [255, 0, 152]
-
-def getColorsForDmx():
+def getCryptoParty(palette):
+  #CryptoParty colors: purple(217/0/255 | d900ff) && pink(255/0/152 | ff0098)
+  cryptoPartyPurple = [217, 0, 255]
+  cryptoPartyPink   = [255, 0, 152]
   return {
     'b': cryptoPartyPink, # base color, usually not utilized
     'c': cryptoPartyPurple, # columns
@@ -22,26 +15,43 @@ def getColorsForDmx():
     'v4': cryptoPartyPurple, # gate
   }
 
+def handleEvents(events, palette):
+  for event in events:
+    if event['summary'].lower() == 'cryptoparty':
+      return getCryptoParty(palette)
+  # Default to farbgeber
+  return palette
 
-class DetectCryptoParty(msgflo.Participant):
+class EventColors(msgflo.Participant):
   def __init__(self, role):
     d = {
-      'component': 'c-flo/DetectCryptoParty',
-      'label': 'Detect a CryptoParty event',
-      'icon': 'unlock-alt ',
+      'component': 'c-flo/EventColors',
+      'label': 'Produce color schemes for events',
+      'icon': 'palette',
       'inports': [
-        { 'id': 'current', 'type': 'array'}
+        { 'id': 'current', 'type': 'array'},
+        { 'id': 'palette', 'type': 'object' },
       ],
       'outports': [
         { 'id': 'palette', 'type': 'object' },
       ],
     }
+    self.palette = None
+    self.current_events = []
     msgflo.Participant.__init__(self, d, role)
 
   def process(self, inport, msg):
-    if len(msg.data) and hasCryptoParty(msg.data):
-      self.send('palette', getColorsForDmx())
-    self.ack(msg)
+    if inport == 'current':
+      self.current_events = msg.data
+      if self.palette:
+        self.send('palette', handleEvents(msg.data, self.palette))
+      self.ack(msg)
+      return
+    if inport == 'palette':
+      self.palette = msg.data
+      self.send('palette', handleEvents(msg.current_events, msg.data))
+      self.ack(msg)
+      return
 
 if __name__ == '__main__':
-  msgflo.main(DetectCryptoParty)
+  msgflo.main(EventColors)
